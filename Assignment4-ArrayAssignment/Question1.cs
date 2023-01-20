e04b1d2cd1fb1e7ac122183bacb57fa98bdc8233
Shubham Patil <118250165+SamSpace2497@users.noreply.github.com>
2023-01-20 10:33:10 +0530

    Add files via upload

---
+++ Assignment4-ArrayAssignment/Question1.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace Assignment4
+{
+    internal class Question1
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Enter No of Batches...");
+            int Batches = Convert.ToInt32(Console.ReadLine());
+            
+            int[][] YCP = new int[Batches][];
+
+            for(int i = 0; i < YCP.Length; i++)
+            {
+                Console.WriteLine($"Enter No of Students for Batch {i + 1} : ");
+                int Students= Convert.ToInt32(Console.ReadLine());
+                YCP[i] = new int[Students]; 
+                for (int j = 0; j < YCP[i].Length; j++)
+                {
+                    Console.WriteLine($"Enter Marks for Student {j + 1} : ");
+                    int Marks = Convert.ToInt32(Console.ReadLine());
+                    YCP[i][j] = Marks;  
+                }
+                Console.WriteLine(" ");
+            }
+            
+            for(int i = 0; i < YCP.Length; i++)
+            {
+                Console.WriteLine($"--------------------------Batch {i+1}----------------------------");
+                for(int j = 0; j < YCP[i].Length; j++)
+                {
+                    Console.WriteLine($"\t \t Student {j + 1} has {YCP[i][j]} Marks...");
+                }
+                Console.WriteLine("-------------------------------------------------------------");
+            }
+
+        }
+    }
+}
---
+++ Assignment4-ArrayAssignment/Question2.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Question2
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter No of Employees...");
+            int TotalEmp = Convert.ToInt32(Console.ReadLine());
+            Employee[] employees = new Employee[TotalEmp];
+
+            for (int i = 0; i < employees.Length; i++)                          //Add Employees In Array
+            {
+                employees[i] = new Employee();
+
+                Console.WriteLine("Enter Employee Name...");
+                employees[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Enter Employee Salary...");
+                employees[i].Salary = Convert.ToDecimal(Console.ReadLine());
+
+            }
+            foreach (Employee employee in employees)                          //Display Employees From Array
+            {
+                Console.WriteLine($"-----------------------EMPLOYEE {employee.EmpNo}--------------------------");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(employee.ToString());
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("-----------------------------------------------------------");
+
+            }
+            Console.WriteLine(" ");
+
+            //Employee With Highest Sal With 2 For Loops
+            /*decimal highest = 0;                                               
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (highest < employees[i].Salary)
+                {
+                    highest = employees[i].Salary;
+                }
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].Salary == highest)
+                {
+
+                    Console.WriteLine("-----------------------------------------------------------");
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"Employee With Highest Salary :({employees[i].EmpNo}-{employees[i].Name}-Rs.{employees[i].Salary})");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine("-----------------------------------------------------------");
+                }
+            }*/
+
+            //Finding highest sal using Variable declared in Employee Class
+            /*for (int i = 0; i < employees.Length; i++)                              
+            {
+                if (employees[i].Salary == Employee.highest)
+                {
+
+                    Console.WriteLine("-----------------------------------------------------------");
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"Employee With Highest Salary :({employees[i].EmpNo}-{employees[i].Name}-Rs.{employees[i].Salary})");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine("-----------------------------------------------------------");
+                }
+            }*/
+
+            //Finding Highest sal using single for loop
+            decimal highest = 0;                                              
+            Employee temp = new Employee();
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (highest < employees[i].Salary)
+                {
+                    highest = employees[i].Salary;
+                    temp = employees[i];
+                }
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"Employee With Highest Salary :({temp.EmpNo}-{temp.Name}-Rs.{temp.Salary})");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("-----------------------------------------------------------");
+
+            //Finding Employee through employee number by using Delegate
+            Action<int> o1 = (EmpNo) =>                                       
+            {
+                for (int i = 0; i < employees.Length; i++)
+                {
+                    if (employees[i].EmpNo == EmpNo)
+                    {
+                        Console.WriteLine("-----------------------------------------------------------");
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine($"Employee :{employees[i].EmpNo}-{employees[i].Name}-Rs.{employees[i].Salary}");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.WriteLine("-----------------------------------------------------------");
+                    }
+                }
+            };
+            o1(2);
+        }
+    }
+    public class Employee
+    {
+        private static int index = 0;
+        private readonly int empNo;
+        //public static decimal highest = 0;
+        public int EmpNo
+        {
+            get
+            {
+                return empNo;
+            }
+        }
+        public string Name
+        {
+            get; set;
+        }
+        private decimal salary;
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                salary = value;
+                /*if (highest < salary)
+                {
+                    highest = Salary;
+                }*/
+            }
+        }
+        public Employee(String Name = "No Name", decimal Salary = 0)
+        {
+            index++;
+            empNo = index;
+            this.Name = Name;
+            this.Salary = Salary;
+        }
+        public override String ToString()
+        {
+            return $"EMPLOYEE : {EmpNo}, NAME : {Name}, SALARY : Rs.{Salary}";
+        }
+    }
+}
+
